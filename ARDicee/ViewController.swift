d5270f09c753b83d1a39cb8567f3bd4bf594717d
metalnodeug <metalnodeug@gmail.com>
2019-09-14 17:41:25 +0200

    Create the first dice red in AR

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -21,13 +21,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.showsStatistics = false
+
+        //        //Create a 3D cube with size
+        //        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        //
+        //        // Create the texture
+        //        let material = SCNMaterial()
+        //
+        //        //Assign a color to the material
+        //        material.diffuse.contents = UIColor.red
+        //
+        //        //Assign the material to the cube in array
+        //        cube.materials = [material]
+        //
+        //        //Create a spacial point
+        //        let node = SCNNode()
+        //
+        //        //Choose a position for the node
+        //        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        //
+        //        //Assigning geometry to the node
+        //        node.geometry = cube
+        //
+        //        //Adding to the scene the node created
+        //        sceneView.scene.rootNode.addChildNode(node)
+        //
+        //        //Adding light for more 3D effect impression
+        //        sceneView.autoenablesDefaultLighting = true
+
+        let diceScene = SCNScene(named: "art.scnassets/DiceTexture.scn")!
+
+        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+
+            diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1 )
+
+            sceneView.scene.rootNode.addChildNode(diceNode)
+
+            sceneView.autoenablesDefaultLighting = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +67,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -49,14 +83,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
-    }
-*/
+     return node
+     }
+     */
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
